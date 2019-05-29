43a3846a8ce768d4f2ebe120de39ca59d5810589
simon-schaefer <sischaef@student.ethz.ch>
2019-05-29 17:55:30 +0200

    qvalues returning to task step function

--- baselines/deepq/deepq.py
+++ baselines/deepq/deepq.py
@@ -277,10 +277,11 @@ def learn(env,
                 kwargs['reset'] = reset
                 kwargs['update_param_noise_threshold'] = update_param_noise_threshold
                 kwargs['update_param_noise_scale'] = True
-            action = act(np.array(obs)[None], sample_probs, update_eps=update_eps, **kwargs)[0]
+            action = act(np.array(obs)[None], sample_probs, update_eps=update_eps, **kwargs)
+            action, qvalues = action
             env_action = action
             reset = False
-            new_obs, rew, done, _ = env.step(env_action)
+            new_obs, rew, done, _ = env.step((env_action, qvalues))
             new_sample_probs = env.envs[0].get_action_probabilities()
             # Store transition in the replay buffer.
             replay_buffer.add(obs, action, rew, new_obs, float(done))
